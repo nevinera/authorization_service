5d01627f45341e4c376b5f394c98cbd54eb99871
Eric Mueller <nevinera@gmail.com>
2017-12-02 14:04:10 -0500

    implement User put/get/delete in-place

--- server.go
+++ server.go
@@ -1,17 +1,67 @@
 package main
 
 import (
-  "fmt"
+  "log"
+  "strconv"
   "net/http"
-  "./handlers"
+  "time"
+  "encoding/json"
+  "github.com/gorilla/mux"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Fake HTTP response to `%s`", r.URL.Path)
+type User struct {
+  Uuid string
+  CreatedAt time.Time
+}
+
+func sendError(w http.ResponseWriter, status int, msg string) {
+  w.WriteHeader(status)
+  errorData := map[string]string{
+    "error": msg,
+    "status": strconv.Itoa(status),
+    "occurred_at": time.Now().String(),
+  }
+  json.NewEncoder(w).Encode(errorData)
+}
+
+var users = make(map[string]User)
+
+func GetUser(w http.ResponseWriter, r *http.Request) {
+  params := mux.Vars(r)
+  if user, included := users[params["uuid"]]; included {
+    w.WriteHeader(http.StatusOK)
+    json.NewEncoder(w).Encode(user)
+  } else {
+    sendError(w, http.StatusNotFound, "That user was not found")
+  }
+}
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+  params := mux.Vars(r)
+  if _, included := users[params["uuid"]]; included {
+    sendError(w, http.StatusConflict, "That user already exists")
+  } else {
+    user := User{Uuid: params["uuid"], CreatedAt: time.Now()}
+    users[params["uuid"]] = user
+    w.WriteHeader(http.StatusCreated)
+    json.NewEncoder(w).Encode(user)
+  }
+}
+
+func DestroyUser(w http.ResponseWriter, r *http.Request) {
+  params := mux.Vars(r)
+  if user, included := users[params["uuid"]]; included {
+    delete(users, params["uuid"])
+    json.NewEncoder(w).Encode(user)
+  } else {
+    sendError(w, http.StatusNotFound, "That user was not found")
+  }
 }
 
 func main() {
-  http.HandleFunc("/", handler)
-  http.Handle("/users/", handlers.UsersHandler{})
-  http.ListenAndServe(":3000", nil)
+  router := mux.NewRouter()
+  router.HandleFunc("/users/{uuid}", GetUser).Methods("GET")
+  router.HandleFunc("/users/{uuid}", CreateUser).Methods("PUT")
+  router.HandleFunc("/users/{uuid}", DestroyUser).Methods("DELETE")
+  log.Fatal(http.ListenAndServe(":3000", router))
 }
